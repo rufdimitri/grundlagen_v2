d243c8b15002789c86375f410769132bc57fe39e
rufdimitri <ruf.dimitri.de@gmail.com>
2023-11-07 14:42:54 +0100

    loop with multiple variable

---
+++ src/rd/loop/Main.java
@@ -0,0 +1,12 @@
+package rd.loop;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Schleife mit 2 variablen
+        for (int i = 0, j = 0; i < 3; i++, j+=2) {
+            System.out.println("i = " + i);
+            System.out.println("j = " + j);
+        }
+    }
+}
