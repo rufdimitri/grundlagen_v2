2b8063bd3c90ed9a0634d178380f73e27b2f8e91
rufdimitri <ruf.dimitri.de@gmail.com>
2023-11-08 14:36:14 +0100

    Tannenbaum is colored with ConsoleColors

--- src/rd/tannenbaum/Main.java
+++ src/rd/tannenbaum/Main.java
@@ -1,16 +1,27 @@
 package rd.tannenbaum;
 
+import rd.console_colors.ConsoleColors;
+
 public class Main {
     public static void main(String[] args) {
         int baumgroesse = 21;
         int leerzeichenAnzahl = baumgroesse/2;
         for (int sternchenAnzahl = 1; sternchenAnzahl <= baumgroesse; sternchenAnzahl+=2, leerzeichenAnzahl--) {
+
+            System.out.print(ConsoleColors.CYAN_BACKGROUND);
             for (int s = 0; s < leerzeichenAnzahl; s++) {
                 System.out.print(" ");
             }
+            System.out.print(ConsoleColors.GREEN_BACKGROUND);
             for (int s = 0; s < sternchenAnzahl; s++) {
                 System.out.print("*");
             }
+            System.out.print(ConsoleColors.CYAN_BACKGROUND);
+            for (int s = 0; s < leerzeichenAnzahl; s++) {
+                System.out.print(" ");
+            }
+            System.out.print(ConsoleColors.RESET);
+
             System.out.println();
         }
     }
