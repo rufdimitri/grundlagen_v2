72b5761b557488c13e9b2b44a06bc6c0cdd553ba
rufdimitri <ruf.dimitri.de@gmail.com>
2023-11-17 10:20:50 +0100

    identityHashcode add description

--- src/rd/identity_hashcode/A_super.java
+++ src/rd/identity_hashcode/A_super.java
@@ -1,8 +1,8 @@
 package rd.identity_hashcode;
 
-public abstract class A  {
-    public A() {
-        System.out.format("Constructor A --- identityHashCode(hex): %s --- toString: %s \n"
+public abstract class A_super {
+    public A_super() {
+        System.out.format("Constructor A_super --- identityHashCode(hex): %s --- toString: %s \n"
                 , Integer.toHexString(System.identityHashCode(this))
                 , this.toString()
         );
