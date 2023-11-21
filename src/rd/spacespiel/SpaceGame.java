6112185130d1f4f533324297706fd4107218e083
rufdimitri <ruf.dimitri.de@gmail.com>
2023-11-21 13:43:05 +0100

    Spacespiel

--- src/rd/spacespiel/SpaceGame.java
+++ src/rd/spacespiel/SpaceGame.java
@@ -2,47 +2,65 @@ package rd.spacespiel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.geom.Point2D;
 
-public class SpaceGame extends JPanel {
+public class SpaceGame extends JPanel implements ActionListener {
 
     private Spaceship spaceship = new Spaceship(new Point2D.Double(40, 40), 40);
     private Color background;
+    private boolean isLeftPressed, isRightPressed, isUpPressed, isDownPressed;
+    private double speedX = 0;
+    private double speedY = 0;
+    private double maxSpeed = 20;
+    private double acceleration = 1.0;
+    private int delay = 40;
+    private Timer timer = new Timer(delay, this);
+    private int width, height;
+
+
     public SpaceGame(int w, int h) {
+        width = w;
+        height = h;
         this.background = StartWindow.backgroundColor;
         setBackground(background);
         setFocusable(true);
         requestFocusInWindow();
-        addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-
-            }
-
+        addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-                if (e.getKeyChar() == 'w') {
-                    SpaceGame.this.spaceship.position.y += -10;
+                super.keyPressed(e);
+                int key = e.getKeyCode();
+                if (key == KeyEvent.VK_LEFT) {
+                    isLeftPressed = true;
+                } else if (key == KeyEvent.VK_UP) {
+                    isUpPressed = true;
+                } else if (key == KeyEvent.VK_RIGHT) {
+                    isRightPressed = true;
+                } else if (key == KeyEvent.VK_DOWN) {
+                    isDownPressed = true;
                 }
-                if (e.getKeyChar() == 's') {
-                    SpaceGame.this.spaceship.position.y += 10;
-                }
-                if (e.getKeyChar() == 'a') {
-                    SpaceGame.this.spaceship.position.x += -10;
-                }
-                if (e.getKeyChar() == 'd') {
-                    SpaceGame.this.spaceship.position.x += 10;
-                }
-                repaint();
             }
 
             @Override
             public void keyReleased(KeyEvent e) {
-                System.out.println("release");
+                super.keyReleased(e);
+                int key = e.getKeyCode();
+                if (key == KeyEvent.VK_LEFT) {
+                    isLeftPressed = false;
+                } else if (key == KeyEvent.VK_UP) {
+                    isUpPressed = false;
+                } else if (key == KeyEvent.VK_RIGHT) {
+                    isRightPressed = false;
+                } else if (key == KeyEvent.VK_DOWN) {
+                    isDownPressed = false;
+                }
             }
         });
+        timer.start();
     }
 
     @Override
@@ -73,5 +91,59 @@ public class SpaceGame extends JPanel {
         g2d.fillPolygon(pointsX2, pointsY2, pointsX2.length);
         g2d.setColor(Color.CYAN);
         g2d.drawPolygon(pointsX2, pointsY2, pointsX2.length);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (isLeftPressed || isRightPressed) {
+            if (isLeftPressed) {
+                speedX -= acceleration;
+                if(speedX < -maxSpeed) {
+                    speedX = -maxSpeed;
+                }
+            }
+            if (isRightPressed) {
+                speedX += acceleration;
+                if(speedX > maxSpeed) {
+                    speedX = maxSpeed;
+                }
+            }
+        } else {
+            speedX *= 0.9; //if no keys pressed, automatic slow down
+        }
+        if (isUpPressed || isDownPressed) {
+            if (isUpPressed) {
+                speedY -= acceleration;
+                if(speedY < -maxSpeed) {
+                    speedY = -maxSpeed;
+                }
+            }
+            if (isDownPressed) {
+                speedY += acceleration;
+                if(speedY > maxSpeed) {
+                    speedY = maxSpeed;
+                }
+            }
+        } else {
+            speedY *= 0.9;  //if no keys pressed, automatic slow down
+        }
+        System.out.println(speedX + " speed " + speedY);
+        //speedY += acceleration/2; //gravitation
+        spaceship.position.x += speedX;
+        spaceship.position.y += speedY;
+        if (spaceship.position.x < 0) {
+            spaceship.position.x += width;
+        }
+        if (spaceship.position.x > width) {
+            spaceship.position.x -= width;
+        }
+        if (spaceship.position.y < 0) {
+            spaceship.position.y += height;
+        }
+        if (spaceship.position.y > height) {
+            spaceship.position.y -= height;
+        }
+        System.out.println(speedX + " speed " + speedY);
+        repaint();
     }
 }
