21da98fbdfbdf556eacd0d3b6e8f41e6b7c0969b
rufdimitri <ruf.dimitri.de@gmail.com>
2024-01-04 15:16:07 +0100

    JSON MAP /TODO

---
+++ src/rd/json_marshall/map_to_json/JsonUtil.java
@@ -0,0 +1,68 @@
+package rd.json_marshall.map_to_json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+public class JsonUtil <T> {
+    public Class<T> objectClass;
+
+    private JsonUtil(Class<T> objectClass) {
+        this.objectClass = objectClass;
+    }
+
+    public static <T> JsonUtil<T> of (Class<T> objectClass) {
+        return new JsonUtil<>(objectClass);
+    }
+
+    public T unmarshallFromFile(String fileName) {
+        try (Reader reader = new FileReader(fileName, StandardCharsets.UTF_8)) {
+            GsonBuilder builder = new GsonBuilder();
+            builder.setPrettyPrinting();
+            Gson gson = builder.create();
+
+            return gson.fromJson(reader, objectClass);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public T unmarshallFromFileOrDefault(String fileName, T defaultValue) {
+        try {
+            return unmarshallFromFile(fileName);
+        } catch (Exception e) {
+            return defaultValue;
+        }
+    }
+
+    public void marshallToFile(String fileName, T object) {
+        try (Writer writer = new FileWriter(fileName, StandardCharsets.UTF_8)) {
+            GsonBuilder builder = new GsonBuilder();
+            builder.setPrettyPrinting();
+            Gson gson = builder.create();
+
+            gson.toJson(object, writer);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String marshallMapToString(T object) {
+        Type typeObject = new TypeToken<HashMap>() {}.getType();
+        try (Writer writer = new FileWriter(fileName, StandardCharsets.UTF_8)) {
+            GsonBuilder builder = new GsonBuilder();
+            builder.setPrettyPrinting();
+            Gson gson = builder.create();
+
+            gson.toJson(object, writer);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/rd/json_marshall/map_to_json/MainInMapToJson.java
@@ -0,0 +1,9 @@
+package rd.json_marshall.map_to_json;
+
+public class MainInMapToJson {
+    public static void main(String[] args) {
+        //TODO
+        String jsonString = JsonUtil.of().marshallMapToString();
+    }
+
+}
